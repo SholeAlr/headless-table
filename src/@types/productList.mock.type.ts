38ae386c3bbd66decd0cfc809f7152688eb5a960
Shole Alirezaei <alirezaei.sholeh@golpakhsheaval.com>
2024-09-24 14:36:03 +0330

    create type for product item list

---
+++ src/@types/productList.mock.type.ts
@@ -0,0 +1,37 @@
+export type ProductListItem = {
+  id: number
+  productName: string
+  code: number
+  distributerId: number
+  distributer: string
+  distributerImage: string
+  productId: number
+  productImage: string
+  productNoInPack: number
+  netWeight: number
+  originCategoryId: number
+  originCategory: string
+  subCategoryId: number
+  subCategory: string
+  categoryId: number
+  category: string
+  smallUnitId: number
+  smallUnit: string
+  bigUnitId: number
+  bigUnit: string
+  barcode: string
+  brandId: number
+  brandName: string
+  brandImage: string
+  price: number
+  consumerPrice: number
+  minOrder: number
+  maxOrder: number
+  discount: null
+  tieredDiscount: null
+  tax: number
+  score: null
+  quantity: number
+  saleWithBigUnit: boolean
+  includePromotion: boolean
+}
