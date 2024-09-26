163ef0f73557e3c9f3ae6af63e5c9306739aec7b
Shole Alirezaei <alirezaei.sholeh@golpakhsheaval.com>
2024-09-26 10:38:58 +0330

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+import colors from './src/config/colors'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: colors,
+    },
+  },
   plugins: [],
 }
