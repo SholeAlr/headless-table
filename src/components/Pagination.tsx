c2db72cf61436bc25e457f4556f69340d1fe66ab
sholealirezaei <sh.alirezaii@gmail.com>
2024-10-02 11:48:29 +0330

    make a component for pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export const Pagination = ({ table }: any) => {
+  return (
+    <div className='flex items-center gap-2 text-center'>
+      <button
+        className='border rounded p-1'
+        onClick={() => table.setPageIndex(0)}
+        disabled={!table.getCanPreviousPage()}
+      >
+        {'<<'}
+      </button>
+      <button
+        className='border rounded p-1'
+        onClick={() => table.previousPage()}
+        disabled={!table.getCanPreviousPage()}
+      >
+        {'<'}
+      </button>
+      <button
+        className='border rounded p-1'
+        onClick={() => table.nextPage()}
+        disabled={!table.getCanNextPage()}
+      >
+        {'>'}
+      </button>
+      <button
+        className='border rounded p-1'
+        onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+        disabled={!table.getCanNextPage()}
+      >
+        {'>>'}
+      </button>
+      <span className='flex items-center gap-1'>
+        <div>صفحه</div>
+        <strong>
+          {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </strong>
+      </span>
+      <span className='flex items-center gap-1'>
+        | برو به صفحه:
+        <input
+          type='number'
+          min='1'
+          max={table.getPageCount()}
+          defaultValue={table.getState().pagination.pageIndex + 1}
+          onChange={(e) => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0
+            table.setPageIndex(page)
+          }}
+          className='border p-1 rounded w-16'
+        />
+      </span>
+      <select
+        value={table.getState().pagination.pageSize}
+        onChange={(e) => {
+          table.setPageSize(Number(e.target.value))
+        }}
+      >
+        {[10, 20, 30, 40, 50].map((pageSize) => (
+          <option key={pageSize} value={pageSize}>
+            {pageSize}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,6 +10,7 @@ import { productList } from '../../mock/productList'
 import { Table } from '../../components/Table'
 import { useCreateTable } from '../../hooks/useCreateTable'
 import { useCreateProductTableColumns } from './hooks/useCreateProductTable'
+import { Pagination } from '../../components/Pagination'
 
 declare module '@tanstack/react-table' {
   //allows us to define custom properties for our columns
@@ -99,69 +100,8 @@ export const Home = () => {
   return (
     <div className='w-screen overflow-x-scroll'>
       <Table table={table} />
-      <div className='flex items-center gap-2 text-center'>
-        <button
-          className='border rounded p-1'
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {'<<'}
-        </button>
-        <button
-          className='border rounded p-1'
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {'<'}
-        </button>
-        <button
-          className='border rounded p-1'
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {'>'}
-        </button>
-        <button
-          className='border rounded p-1'
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {'>>'}
-        </button>
-        <span className='flex items-center gap-1'>
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{' '}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className='flex items-center gap-1'>
-          | برو به صفحه:
-          <input
-            type='number'
-            min='1'
-            max={table.getPageCount()}
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              table.setPageIndex(page)
-            }}
-            className='border p-1 rounded w-16'
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value))
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
+
+      <Pagination table={table} />
       <div className='flex justify-between'>
         <div>{table.getPrePaginationRowModel().rows.length} ردیف</div>
         <div>
